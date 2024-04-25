fc136b1869ec646ecdd4476cde856e8d51252298
zthomas18 <zthomas18@ivytech.edu>
2024-04-24 20:15:09 -0400

    class and function concept layout

--- blank.py
+++ blank.py
@@ -1,1 +1,16 @@
-#This is a blank file to practice pushing to git
+class Invoice:
+    def __init__():
+    def get_invoice_summary():
+
+class InvoiceStorage:
+    def __init__():
+    def store_invoice():
+    def retrieve_invoice():
+class InvoiceManager:
+    def __init__():
+    def create_invoice():
+    def get_invoice():
+    def generate_invoice_id():
+
+invoice_summary = get_invoice(invoice_id)
+print(invoice_summary)
